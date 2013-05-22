bcebdefd2117265d11470117f98dd73043c7c977
Rich E <rich.eakin@gmail.com>
2013-05-22 17:58:18 -0400

    reduce default busses in Mixer to 10

--- src/audio2/Graph.h
+++ src/audio2/Graph.h
@@ -142,7 +142,7 @@ class BufferTap : public Node {
 
 class Mixer : public Node {
   public:
-	Mixer() : Node(), mMaxNumBusses( 20 ) { mSources.resize( mMaxNumBusses ); }
+	Mixer() : Node(), mMaxNumBusses( 10 ) { mSources.resize( mMaxNumBusses ); }
 	virtual ~Mixer() {}
 
 	virtual void connect( NodeRef source );
