c595bfd20a86ca30d87787de62f4c457a7f219ea
Rich E <rich.eakin@gmail.com>
2013-05-29 11:53:02 -0400

    minor formatting

--- src/audio2/msw/util.h
+++ src/audio2/msw/util.h
@@ -6,26 +6,24 @@ namespace audio2 { namespace msw {
 
 struct ComReleaser {
 	template <typename T>
-	void operator()(T* ptr) {
-		ptr->Release();
-	}
+	void operator()(T* ptr)	{ ptr->Release(); }
 };
 
 //! Creates a unique_ptr whose deleter will properly decrement the reference count of a COM object
 // TODO: move to cinder's msw utils
 template<typename T>
-inline std::unique_ptr<T, ComReleaser> makeComUnique( T *p )
-{ return std::unique_ptr<T, ComReleaser>( p ); }
+inline std::unique_ptr<T, ComReleaser> makeComUnique( T *p )	{ return std::unique_ptr<T, ComReleaser>( p ); }
 
 struct VoiceDeleter {
 	template <typename T>
-	void operator()( T *voice ) {
-		voice->DestroyVoice();
-	}
+	void operator()( T *voice )	{ voice->DestroyVoice(); }
 };
 
 template<typename T>
-inline std::unique_ptr<T, VoiceDeleter> makeVoiceUnique( T *p )
-{ return std::unique_ptr<T, VoiceDeleter>( p ); }
+inline std::unique_ptr<T, VoiceDeleter> makeVoiceUnique( T *p )	{ return std::unique_ptr<T, VoiceDeleter>( p ); }
+
+
+
+
 
 } } // namespace audio2::msw
