61256979e04677732f94e7b9f5053eb9383e0b6f
Rich E <rich.eakin@gmail.com>
2013-06-23 17:48:06 -0400

    wrap BOOST_ASSERT_MSG -> CI_ASSERT_MSG

--- src/audio2/assert.h
+++ src/audio2/assert.h
@@ -2,4 +2,5 @@
 
 #include <boost/assert.hpp>
 
-#define CI_ASSERT(expr) BOOST_ASSERT(expr)
+#define CI_ASSERT( expr ) BOOST_ASSERT( expr )
+#define CI_ASSERT_MSG( expr, msg ) BOOST_ASSERT_MSG(expr, msg )
