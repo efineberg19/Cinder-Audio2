644349e93eedf0df09b7f5d0fae973d7a8a4f133
Rich E <rich.eakin@gmail.com>
2013-08-31 20:13:15 -0400

    had to move unit .cpp's to .h's, else require boost.test.lib

---
+++ test/unit/src/FftUnit.h
@@ -0,0 +1,12 @@
+#include "audio2/Fft.h"
+
+BOOST_AUTO_TEST_SUITE( test_fft )
+
+using namespace ci::audio2;
+
+BOOST_AUTO_TEST_CASE( test_blarg )
+{
+    BOOST_REQUIRE_EQUAL( 4 + 4, 8 );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
