7343425993339a13429f5d984799f793d2d8806e
Rich E <rich.eakin@gmail.com>
2013-09-18 02:08:29 -0400

    use BOOST_CURRENT_FUNCTION for debug function string

--- src/audio2/Debug.h
+++ src/audio2/Debug.h
@@ -24,16 +24,11 @@
 #pragma once
 
 #include "audio2/CinderAssert.h"
+#include <boost/current_function.hpp>
 
 // note: this file will be removed at a later point
 
 #include "cinder/app/App.h"
 
-#if defined( CINDER_COCOA )
-	#define LOG_FUNCTION_CALL __PRETTY_FUNCTION__
-#else
-	#define LOG_FUNCTION_CALL __FUNCTION__
-#endif
-
-#define LOG_V ci::app::console() << LOG_FUNCTION_CALL << " | "
+#define LOG_V ci::app::console() << BOOST_CURRENT_FUNCTION << " | "
 #define LOG_E LOG_V << __LINE__ << " | " << " *** ERROR *** : "
