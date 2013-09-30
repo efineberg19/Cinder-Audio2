d2cc9b9680350da4c792ab93eaae84d22694f0e0
Rich E <rich.eakin@gmail.com>
2013-09-29 23:55:58 -0400

    added a mono mp3, which thankfully seems to work

--- test/FileNodeTest/include/Resources.h
+++ test/FileNodeTest/include/Resources.h
@@ -5,5 +5,6 @@
 #define RES_TONE440L220R_WAV			CINDER_RESOURCE( ../../../assets/, tone440L220R.wav, 129, SOUND )
 #define RES_TONE440L220R_FLOAT_WAV		CINDER_RESOURCE( ../../../assets/, tone440L220R_float.wav, 130, SOUND )
 
-#define RES_TONE440L220R_MP3			CINDER_RESOURCE( ../../../assets/, tone440L220R.mp3, 131, SOUND )
-#define RES_CASH_MP3				CINDER_RESOURCE( ../../../assets/, cash_satisfied_mind.mp3, 132, SOUND )
+#define RES_TONE440_MP3					CINDER_RESOURCE( ../../../assets/, tone440.mp3, 132, SOUND )
+#define RES_TONE440L220R_MP3			CINDER_RESOURCE( ../../../assets/, tone440L220R.mp3, 133, SOUND )
+#define RES_CASH_MP3					CINDER_RESOURCE( ../../../assets/, cash_satisfied_mind.mp3, 134, SOUND )
