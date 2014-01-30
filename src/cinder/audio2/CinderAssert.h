eea82f417103f3d4e9215f4bc5ffd958bc2dde0b
Rich E <rich.eakin@gmail.com>
2014-01-30 16:44:56 -0500

    added CI_VERIFY / CI_VERIFY_MSG macros

--- src/cinder/audio2/CinderAssert.h
+++ src/cinder/audio2/CinderAssert.h
@@ -23,8 +23,14 @@
 
 // This file has no include guards, so users can redefine the behavior of assertions per compilation unit
 //
+// Included macros:
+// - CI_ASSERT( expr ): asserts that \a expr evaluates to true. By default it is equivalent to assert( expr )
+// - CI_ASSERT_MSG( expr, msg ): same as CI_ASSERT but takes an additional, human readable \a const char* message parameter
+// - CI_VERIFY( expr ): same as CI_ASSERT, but still evaluates \a expr in release mode.
+// - CI_VERIFY_MSG( expr ): same as CI_ASSERT_MSG, but still evaluates \a expr in release mode.
+//
 // User-definable parameters:
-// - CI_DISABLE_ASSERTS: disables all asserts, they become no-op's.
+// - CI_DISABLE_ASSERTS: disables all asserts, they become no-ops (VERIFY variants still evaluate \a expr).
 // - CI_ENABLE_ASSERT_HANDLER: if this is set, users must define assertionFailed() and assertionFailedMessage()
 //	 to handle a failed assertion.
 // - CI_ASSERT_DEBUG_BREAK: overrides default assertion behavoir to break into the debugger instead of
@@ -92,9 +98,15 @@
 
 	#endif // defined( CI_ASSERT_DEBUG_BREAK )
 
+
+	#define CI_VERIFY( expr )			CI_ASSERT( expr )
+	#define CI_VERIFY_MSG( expr, msg )	CI_ASSERT_MSG( expr, msg )
+
 #else
 
-	#define CI_ASSERT(expr) ((void)0)
-	#define CI_ASSERT_MSG( expr, msg ) ((void)0)
+	#define CI_ASSERT( expr )			((void)0)
+	#define CI_ASSERT_MSG( expr, msg )	((void)0)
+	#define CI_VERIFY( expr )			((void)expr)
+	#define CI_VERIFY_MSG( expr, msg )	((void)expr)
 
 #endif
