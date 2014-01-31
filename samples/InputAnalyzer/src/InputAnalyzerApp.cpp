355aa5dc03f890855d8e80bf77298df3ebf1fd2f
Rich E <rich.eakin@gmail.com>
2014-01-31 04:08:13 -0500

    added info to explain what the SpectrumPlot / mag spectrum is showing

--- samples/InputAnalyzer/src/InputAnalyzerApp.cpp
+++ samples/InputAnalyzer/src/InputAnalyzerApp.cpp
@@ -1,8 +1,10 @@
 #include "cinder/app/AppNative.h"
 #include "cinder/gl/gl.h"
+#include "cinder/gl/TextureFont.h"
 
 #include "cinder/audio2/Context.h"
 #include "cinder/audio2/Scope.h"
+#include "cinder/audio2/Utilities.h"
 
 #include "../../../samples/common/AudioDrawUtils.h"
 
@@ -13,13 +15,19 @@ using namespace std;
 class InputAnalyzer : public AppNative {
 public:
 	void setup();
-	void mouseDrag( MouseEvent event ) override;
+	void mouseDown( MouseEvent event );
+	void update();
 	void draw();
+
+	void drawLabels();
+	void printBinInfo( float mouseX );
 
 	audio2::LineInRef			mLineIn;
 	audio2::ScopeSpectralRef	mScopeSpectral;
+	vector<float>				mMagSpectrum;
 
 	SpectrumPlot				mSpectrumPlot;
+	gl::TextureFontRef			mTextureFont;
 };
 
 void InputAnalyzer::setup()
@@ -33,8 +41,6 @@ void InputAnalyzer::setup()
 
 	mLineIn >> mScopeSpectral;
 
-	ctx->printGraph();
-
 	// not currently needed since LineIn is auto-enabled by default.
 	//	- Is this inconsistent? Other NodeInput's default to needing start().
 //	mLineIn->start();
@@ -42,17 +48,58 @@ void InputAnalyzer::setup()
 	ctx->start();
 }
 
-void InputAnalyzer::mouseDrag( MouseEvent event )
+void InputAnalyzer::mouseDown( MouseEvent event )
 {
+	if( mSpectrumPlot.getBounds().contains( event.getPos() ) )
+		printBinInfo( event.getX() );
+}
+
+void InputAnalyzer::update()
+{
+	mSpectrumPlot.setBounds( Rectf( 40, 40, getWindowWidth() - 40, getWindowHeight() - 40 ) );
+
+	mMagSpectrum = mScopeSpectral->getMagSpectrum();
 }
 
 void InputAnalyzer::draw()
 {
-	gl::clear( Color() );
+	gl::clear();
+	gl::enableAlphaBlending();
 
-	const auto &mag = mScopeSpectral->getMagSpectrum();
-	mSpectrumPlot.setBounds( Rectf( 30, 30, getWindowWidth() - 30, getWindowHeight() - 30 ) );
-	mSpectrumPlot.draw( mag );
+	mSpectrumPlot.draw( mMagSpectrum );
+	drawLabels();
+}
+
+void InputAnalyzer::drawLabels()
+{
+	if( ! mTextureFont )
+		mTextureFont = gl::TextureFont::create( Font( Font::getDefault().getName(), 16 ) );
+
+	gl::color( 0, 0.9f, 0.9f );
+
+	// draw x-axis label
+	string freqLabel = "Frequency (hertz)";
+	mTextureFont->drawString( freqLabel, Vec2f( getWindowCenter().x - mTextureFont->measureString( freqLabel ).x / 2, getWindowHeight() - 20 ) );
+
+	// draw y-axis label
+	string dbLabel = "Magnitude (decibels)";
+	gl::pushModelView();
+		gl::translate( 30, getWindowCenter().y + mTextureFont->measureString( dbLabel ).x / 2 );
+		gl::rotate( -90 );
+		mTextureFont->drawString( dbLabel, Vec2f::zero() );
+	gl::popModelView();
+}
+
+void InputAnalyzer::printBinInfo( float mouseX )
+{
+	size_t numBins = mScopeSpectral->getFftSize() / 2;
+	size_t bin = min( numBins - 1, size_t( ( numBins * ( mouseX - mSpectrumPlot.getBounds().x1 ) ) / mSpectrumPlot.getBounds().getWidth() ) );
+
+	float binFreqWidth = mScopeSpectral->getFreqForBin( 1 ) - mScopeSpectral->getFreqForBin( 0 );
+	float freq = mScopeSpectral->getFreqForBin( bin );
+	float mag = audio2::toDecibels( mMagSpectrum[bin] );
+
+	console() << "bin: " << bin << ", freqency (hertz): " << freq << " - " << freq + binFreqWidth << ", magnitude (decibels): " << mag << endl;
 }
 
 CINDER_APP_NATIVE( InputAnalyzer, RendererGl )
